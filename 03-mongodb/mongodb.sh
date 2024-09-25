a0bc8b966f686ba909e2df3601899c4d00694fbf
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-09-25 13:32:03 +0530

    infra standard

---
+++ 03-mongodb/mongodb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /tmp
+git clone https://github.com/VignanBaligari234/roboshop-shell.git
+cd roboshop-shell
+sh mongodb.sh
---
+++ 06-web-alb/main.tf
@@ -0,0 +1,28 @@
+resource "aws_lb" "web_alb" {
+  name               = "${var.project_name}-${var.common_tags.Component}"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [data.aws_ssm_parameter.web_alb_sg_id.value]
+  subnets            = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+
+  #enable_deletion_protection = true
+
+  tags = var.common_tags
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+
+  
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "this is the fixed response from web alb"
+      status_code  = "200"
+    }
+  }
+}
